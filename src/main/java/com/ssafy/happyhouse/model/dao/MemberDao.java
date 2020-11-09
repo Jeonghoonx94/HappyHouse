86ec4506ca81fbcde5c84cb7880baeb950f9a95e
user <user@LAPTOP-HG6B5E45>
2020-11-09 17:32:32 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://lab.ssafy.com/wjdgnsx94/happyhouse_spring.git into member

--- src/main/java/com/ssafy/happyhouse/model/dao/MemberDao.java
+++ src/main/java/com/ssafy/happyhouse/model/dao/MemberDao.java
@@ -4,10 +4,13 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.ssafy.happyhouse.model.MemberDto;
 import com.ssafy.happyhouse.model.PageBean;
 
 
+@Mapper
 public interface MemberDao {
 	public MemberDto login(Map<String, String> map) throws SQLException;
 	
